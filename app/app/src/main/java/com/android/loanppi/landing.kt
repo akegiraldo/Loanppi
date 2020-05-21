d1d7f3f3e69c18656695dcd5a832f57273395eae
Doouh <ake.giraldo@hotmail.com>
2020-05-20 20:27:41 -0500

    Add new activity: signup

--- app/app/src/main/java/com/android/loanppi/landing.kt
+++ app/app/src/main/java/com/android/loanppi/landing.kt
@@ -2,11 +2,18 @@ package com.android.loanppi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.view.View
 
 class landing : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing)
+    }
+
+    fun onSignUp(view: View) {
+        val intent = Intent(this, signup::class.java)
+        startActivity(intent)
     }
 }
