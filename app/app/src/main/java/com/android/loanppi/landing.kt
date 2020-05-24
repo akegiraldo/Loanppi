6ead779ba90a421c9ccd3d175b8ef0a28ee0c95c
Doouh <ake.giraldo@hotmail.com>
2020-05-24 15:49:52 -0500

    Add login view

--- app/app/src/main/java/com/android/loanppi/landing.kt
+++ app/app/src/main/java/com/android/loanppi/landing.kt
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class landing : AppCompatActivity() {
 
@@ -15,6 +20,21 @@ class landing : AppCompatActivity() {
     fun onSignUp(view: View) {
         val intent = Intent(this, signup::class.java)
         startActivity(intent)
+    }
+
+    fun onLogin(view: View) {
+        val builder = AlertDialog.Builder(this)
+        val inflater = layoutInflater
+        val dialogLayout = inflater.inflate(R.layout.dialog_login, null)
+        val title = dialogLayout.findViewById<TextView>(R.id.txt_login_title)
+        val btn_f = dialogLayout.findViewById<Button>(R.id.btn_l_facebook)
+        val btn_g = dialogLayout.findViewById<Button>(R.id.btn_l_google)
+        builder.setView(dialogLayout)
+        builder.setNegativeButton(R.string.cancelar) { dialog, which ->
+            Toast.makeText(applicationContext,
+                R.string.cancelar, Toast.LENGTH_SHORT).show()
+        }
+        builder.show()
     }
 }
 
