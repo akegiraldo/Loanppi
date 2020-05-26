9e872ea578a24d559fade846bf3359a28781472b
Doouh <ake.giraldo@hotmail.com>
2020-05-26 18:00:44 -0500

    Fix some styles

--- app/app/src/main/java/com/android/loanppi/landing.kt
+++ app/app/src/main/java/com/android/loanppi/landing.kt
@@ -1,16 +1,15 @@
 package com.android.loanppi
 
+import android.annotation.SuppressLint
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
-import android.text.Layout
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import kotlinx.android.synthetic.*
 
 class landing : AppCompatActivity() {
 
@@ -30,17 +29,19 @@ class landing : AppCompatActivity() {
         startActivity(intent)
     }
 
+    @SuppressLint("ResourceType")
     fun onLogin(view: View) {
-        val builder = AlertDialog.Builder(this)
+        val builder = AlertDialog.Builder(this, R.style.AlertDialogTheme)
         val inflater = layoutInflater
         val dialogLayout = inflater.inflate(R.layout.dialog_login, null)
         val title = dialogLayout.findViewById<TextView>(R.id.txt_login_title)
         val btn_f = dialogLayout.findViewById<Button>(R.id.btn_l_facebook)
         val btn_g = dialogLayout.findViewById<Button>(R.id.btn_l_google)
-        builder.setView(dialogLayout)
-        builder.setNegativeButton(R.string.cancelar) { dialog, which ->
-            Toast.makeText(applicationContext,
-                R.string.cancelar, Toast.LENGTH_SHORT).show()
+        builder.setView(dialogLayout).setNegativeButton(R.string.cancelar) { dialog, which ->
+            Toast.makeText(
+                applicationContext,
+                R.string.cancelar, Toast.LENGTH_SHORT
+            ).show()
         }
         builder.show()
     }
