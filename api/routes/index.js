d70135228b633b966ce343b2ccf43b6928eddff9
paisap <santiagoaldana58@gmail.com>
2020-06-09 15:54:33 +0000

    rutas

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,13 +3,14 @@
  */
 const express = require('express');
 const router = express.Router()
-const app = require('../app')
 const { helloWorld, searchUSer, NewUser, update, newNeed, options } = require('./gets')
 
 
-app.get('/app/api/v1/', helloWorld)
-app.get('/app/api/v1/user', searchUSer)
-app.post('/app/api/v1/new_user', NewUser)
-app.put('/app/api/v1/update', update)
-app.post('/app/api/v1/lend', newNeed)
-app.get('/app/api/v1/options', options)
+router.get('/app/api/v1/', helloWorld)
+router.get('/app/api/v1/user', searchUSer)
+router.post('/app/api/v1/new_user', NewUser)
+router.put('/app/api/v1/update', update)
+router.post('/app/api/v1/lend', newNeed)
+router.get('/app/api/v1/options', options)
+
+module.exports = router
