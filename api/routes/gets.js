9add054c9c22efe4efce7b767afefeb04f2432c8
andresmelek <candres.isaza@gmail.com>
2020-06-17 23:10:14 +0000

    Updating and adding functions

--- api/routes/gets.js
+++ api/routes/gets.js
@@ -90,9 +90,11 @@ const options = (req, res, next) => {
   list = [];
   const id = req.query.idInvestor;
   Promise.all([availableNeeds(), getAmountStack(id)]).then(response => {
-     list.push(response[0])
-     list.push(response[1][0])
-     res.send(list);
+    if (list[0].length > 0) {
+      list.push(response[0])
+      list.push(response[1][0])
+    }
+    res.send(list);
   }).catch(err => {
       console.error(err);
       res.status(500).send("Not options found");
